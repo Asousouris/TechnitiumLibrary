bddff663ea586ec9af053aa308eb1bbafba0e26c
Shreyas Zare <shreyas@technitium.com>
2024-12-21 20:59:47 +0530

    assembly version updated for release

---
+++ TechnitiumLibrary.Net.Ldap/LdapVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace TechnitiumLibrary.Net.Ldap;
+
+public enum LdapVersion
+{
+    LDAP_VERSION1 = 1,
+    LDAP_VERSION2 = 2,
+    LDAP_VERSION3 = 3,
+
+    LDAP_VERSION_MIN = LDAP_VERSION2,
+    LDAP_VERSION = LDAP_VERSION2,
+    LDAP_VERSION_MAX = LDAP_VERSION3
+}
